43521bfae0b951e2027fb918232db5c838905fd6
lavilao <lavilao570@gmail.com>
2026-03-04 22:25:24 -0500

    Fix icon URL for Evolution

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/evolution.png
+export ICON=https://raw.githubusercontent.com/evolution-project/graphics/master/256x256.png
 export DESKTOP=/usr/share/applications/org.gnome.Evolution.desktop
 
 quick-sharun /usr/bin/evolution
